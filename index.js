e57db4b17bb378a134088cc4439c77543f1e57c5
Zababurin Sergei <zababurins@vk.com>
2022-08-20 02:12:50 +0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import MemStore from './src/mem-store.js'
-import config from './src/config/index.js'
-import connectPeers from './src/connect-peers.js'
-import getIpfsPeerId from './src/get-ipfs-peer-id.js'
-import startIpfs from './src/start-ipfs.js'
-import stopIpfs from './src/stop-ipfs.js'
-import testAPIs from './src/test-apis.js'
-import waitForPeers from './src/wait-for-peers.js'
+import MemStore from './src/mem-store.js';
+import config from './src/config/index.js';
+import connectPeers from './src/connect-peers.js';
+import getIpfsPeerId from './src/get-ipfs-peer-id.js';
+import startIpfs from './src/start-ipfs.js';
+import stopIpfs from './src/stop-ipfs.js';
+import testAPIs from './src/test-apis.js';
+import waitForPeers from './src/wait-for-peers.js';
 
 export {
   MemStore,
